117fba3c1931e23ac30ff26f548dca6837b34a0b
thduongvu <thduong.sarah@gmail.com>
2023-07-19 15:13:19 +0700

    project detail

--- app/project/work/business-travel-management/page.tsx
+++ app/project/work/business-travel-management/page.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import styles from '../../style.module.css'
 
 export default function BusinessTravelMgt() {
   return (
-    <div>Business Travel and Expensive Management</div>
+    <div className={styles.titlespace}>
+        <div className={styles.title}>
+            <div className={styles.titleL}>
+                <h1>Business Travel <br></br>& Expense Management</h1>
+                <p>Quản lý Công tác và Chi phí công tác</p>
+            </div>
+            <div className={styles.titleR}>
+                <p>Bài viết vẫn trong quá trình cập nhật</p>
+                <p>Last updated: Fri, 20 Jan 2023 12:01:41 GMT</p>
+            </div>
+        </div>
+    </div>
   )
 }
